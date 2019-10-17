97e9ac876c02d36d2409c06fbc849b9a557b9474
Tuan Nguyen <anhtuank7c@hotmail.com>
2019-10-18 00:17:26 +0700

    Remove unnecessary modules

--- types.js
+++
@@ -1,5 +0,0 @@
-export type ColorScheme = 'light' | 'light' | 'no-preference'
-export type AppearancePreferences {
-    colorScheme: ColorScheme
-}
-export type AppearanceListener = (preferences: AppearancePreferences) => void
