19192b2ec9a26e2c7a820a69086094a6e4d655cb
Tuan Nguyen <anhtuank7c@hotmail.com>
2019-10-17 23:50:49 +0700

    I need to use this module ASAP so i decided to modify all things except ios code

--- index.js
+++ index.js
@@ -1,5 +1,47 @@
-import { NativeModules } from 'react-native';
+import React, { useMemo } from 'react'
+import { requireNativeComponent, NativeModules, NativeEventEmitter, DeviceEventEmitter, EventSubscription } from 'react-native'
+import { useSubscription } from 'use-subscription'
+import { AppearancePreferences, ColorScheme, AppearanceListener } from './types'
 
-const { Appearance } = NativeModules;
+const RNCAppearance = NativeModules.RNCAppearance
+const EventListener = new NativeEventEmitter(RNCAppearance)
+const RNCAppearanceProvider = requireNativeComponent('RNCAppearanceProvider')
 
-export default Appearance;
+EventListener.addListener('appearanceChanged', (preference: AppearancePreferences) => {
+    Appearance.set(preference)
+})
+
+export const AppearanceProvider = (props: { children: any }) => <RNCAppearanceProvider style={{ flex: 1 }} {...props} />
+let appearancePreferences: AppearancePreferences = RNCAppearance.initialPreferences
+
+export class Appearance {
+    static getColorScheme(): ColorScheme {
+        return appearancePreferences.colorScheme;
+    }
+
+    static set(preferences: AppearancePreferences): void {
+        let { colorScheme } = preferences
+        if (appearancePreferences.colorScheme !== colorScheme) {
+            appearancePreferences = { colorScheme }
+            DeviceEventEmitter.emit('change', preferences)
+        }
+    }
+
+    static addChangeListener(listener: AppearanceListener): EventSubscription {
+        return DeviceEventEmitter.addListener('change', listener)
+    }
+}
+
+export function useColorScheme(): ColorScheme {
+    const subscription = useMemo(
+        () => ({
+            getCurrentValue: () => Appearance.getColorScheme(),
+            subscribe: (callback: AppearanceListener) => {
+                let eventSubscription = Appearance.addChangeListener(callback)
+                return () => eventSubscription.remove()
+            }
+        }), []
+    )
+  
+    return useSubscription<ColorScheme>(subscription);
+}
